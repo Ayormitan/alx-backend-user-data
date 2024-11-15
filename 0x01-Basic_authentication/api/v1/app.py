03f2bf1e2ce3a2af3bfe68de85c551518b827e35
Aayormitan <kolawoleabaz97.com>
2024-11-15 10:17:24 -0800

    App

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -24,15 +24,28 @@ def not_found(error) -> str:
 def Unauthorized(error) -> str:
     """Unauthorized handler
     """
-    return jsonify({"error": "Unauthorized"})
+    return jsonify({"error": "Unauthorized"}), 401
 
 @app.errorhandler(403)
 def Forbidden(error) -> str:
     """ Forbidden handler
     """
-    return jsonify("error": "Forbidden")
+    return jsonify({"error": "Forbidden"}), 403
 
-
+@app.before_request
+def before_request() -> None:
+    """Before request
+    """
+    paths = ['/api/v1/status/', '/api/v1/unathorized',
+            '/api/v1/forbidden/']
+    if not auth:
+        return None
+    if not auth.require_auth(request.path, paths):
+        return None
+    if not auth.authorization_header(request):
+        abort(401)
+    if not auth.current_user(request):
+        abort(403)
 
 if __name__ == "__main__":
     host = getenv("API_HOST", "0.0.0.0")
