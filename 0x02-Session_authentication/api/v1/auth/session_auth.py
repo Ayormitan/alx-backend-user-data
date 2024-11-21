01cdb30a8fdb172bbf77d160f8ccbc9f647d1bb7
Aayormitan <kolawoleabaz97.com>
2024-11-21 09:25:01 -0800

    Session auth

---
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -0,0 +1,12 @@
+#!/usr/bin/pyhon3
+"""Session Auth
+"""
+
+from auth import Auth
+
+class SessionAuth(Auth):
+    """
+    SessionAuth class that inherits from Auth
+    this class is currently empty
+    """
+    pass
